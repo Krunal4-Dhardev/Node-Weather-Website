94e2be6ae80a34509bd0310cf2ebd0a67dc21f2d
Krunal4-Dhardev <krunal4dhardev@gmail.com>
2020-08-01 10:03:36 +0530

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,107 @@
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
+const geocode=require('./utils/geocode')
+const forcast=require('./utils/forcast')
+const request=require('postman-request')
+
+const app=express()
+
+
+//Define Path for express config
+const viewpath=path.join(__dirname,"../templates/views")
+const partialsPath=path.join(__dirname,'../templates/partials')
+
+//seetup handlebars engine and view location 
+app.set('view engine','hbs')
+app.set('views',viewpath    )
+hbs.registerPartials(partialsPath)
+
+//setup static dictnary to serve
+
+app.use(express.static(path.join(__dirname,'../public')))
+
+
+app.get('',(req,res)=>
+{
+    res.render('index',{
+        name:"Krunal Dhardev",
+        nickName:"KD"
+    })
+})
+
+app.get("/help",(req,res)=>{
+    res.render("help",{
+        name:"Krunal Dhardev",
+        help:"More Query Contact on Email :krunal4dhardev@gmail.com"
+    })
+})
+
+app.get("/about",(req,res)=>{
+    res.render("about",{
+        name:"Krunal Dhardev",
+        info:"Web Developer"
+    })
+})
+
+app.get("/weather",(req,res)=>{
+
+    if(!req.query.address)
+    {
+        return res.send({
+            error:"Sorry U have give Name"
+        })
+    }
+
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+        if(error)
+        {
+            res.send({error})
+        }
+
+        forcast(latitude,longitude,(error,forcastdata)=>{
+            console.log(forcastdata)
+            if(error)
+            {
+                res.send(error)
+            }
+            res.send({
+                forcast:forcastdata,
+                location,
+                address:req.query.address
+            })            
+
+
+        })
+
+    })
+
+
+    // res.send({
+    //     forcast:"It is Snowing",
+    //     location:"Rajkot"
+    // })
+})
+
+app.get("/help/*",(req,res)=>
+{
+    res.render("error",{
+        title:" 404 ",
+        name:"Krunal Dhardev",
+        errorMessage:"Help articel not found"
+    })
+})
+
+app.get("*",(req,res)=>{
+    res.render("error",{
+        title:" 404 ",
+        name:"Krunal Dhardev",
+        errorMessage:"Page Not Found"
+    })
+})
+
+
+app.listen(9000, ()=>
+{
+    console.log("Server Is up on port 9000")
+})
---
+++ src/utils/forcast.js
@@ -0,0 +1,26 @@
+const request=require('postman-request')
+const { response } = require('express')
+
+const forcast=(latitude,longitude,callback)=>{
+
+    const url='http://api.weatherstack.com/current?access_key=7d2dd3aea84ff0e1ff570772fa9d005c&query='+latitude+','+longitude+'&units=f'
+
+
+    request({url,json:true},(error,{body}={})=>{
+        if(error)
+        {
+            callback("Unable to connect Weather Services!",undefined)
+        }
+        else if (body.error)
+        {
+            callback("Unable to connect to Loaction Pleae try to another services",undefined)
+        }
+        else
+        {
+            callback(undefined,body.current.weather_descriptions[0]+ ' IT is Currently  '+body.current.temperature+' degress out. it Feellike '+body.current.feelslike+'  Degrees ')
+        }
+
+    })
+}
+
+module.exports=forcast
